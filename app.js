81b92f65fa3c19e06f75142cb895a3af2df3e0cf
jeanbeanie <msjeanneramos@gmail.com>
2018-11-08 17:06:35 +0000

    implement singleton class in app

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require('http');
+const Logger = require('./Logger');
+const Store = require('./Store');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const logger = new Logger().getInstance();
+
+logger.log('Starting app...');
+
+const store = new Store('Jeans Beans', [
+  {
+    item: 'Food',
+    qty: 2,
+    price: 100,
+  },
+]);
+
+logger.log('finished config...');
+console.log(`${logger.count} logs total`);
+logger.logs.map(log => console.log(`${log.message}`));
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
