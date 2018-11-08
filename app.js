aed70726c81c5cc4413fe8dd80e98224147c4bb9
jeanbeanie <msjeanneramos@gmail.com>
2018-11-08 17:57:42 +0000

    added 2 shoppers with redundant shopping lists to main file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const logger = require('./Logger');
 const Store = require('./Store');
+const Shopper = require('./Shopper');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -14,6 +15,23 @@ const store = new Store('Jeans Beans', [
     price: 100,
   },
 ]);
+
+const jean = new Shopper('JeanBean');
+jean.addItemToList('food');
+jean.addItemToList('plants');
+jean.addItemToList('drank');
+jean.addItemToList('coffee');
+jean.addItemToList('The Sims');
+
+const leo = new Shopper('Leo Bae');
+leo.addItemToList('food');
+leo.addItemToList('plants');
+leo.addItemToList('drank');
+leo.addItemToList('coffee');
+leo.addItemToList('Factorio');
+
+logger.log(`${jean.name}: ${jean.shoppingList}`);
+logger.log(`${leo.name}: ${leo.shoppingList}`);
 
 logger.log('finished config...');
 console.log(`${logger.count} logs total`);
